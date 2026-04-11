025d81786ac3120168025b41d647c299da83d638
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2026-04-11 13:22:22 -0300

    Update models.py
    Classificando noticias, unique para evitar duplicações e parametros para data, fonte e titulo

--- backend/noticias/models.py
+++ backend/noticias/models.py
@@ -1,1 +1,10 @@
+from django.db import models
 
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=300)
+    link = models.URLField(unique=True)
+    fonte = models.CharField(max_length=100)
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
