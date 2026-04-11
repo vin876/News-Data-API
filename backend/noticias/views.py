31e83a0a11660189e4cdf3d801f9f40802249d78
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2026-04-11 13:26:10 -0300

    Update views.py
    importante modelos para noticias, serializer, estruturando mais a api view e definindo a função listar noticias

--- backend/noticias/views.py
+++ backend/noticias/views.py
@@ -1,1 +1,13 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Noticia
+from .serializers import NoticiaSerializer
 
+@api_view(['GET'])
+def listar_noticias(request):
+
+    noticias = Noticia.objects.all()
+
+    serializer = NoticiaSerializer(noticias, many=True)
+
+    return Response(serializer.data)
