c8f3c70db7a74dcb7e15ed0f99f1bbd44fb61a59
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2026-04-11 13:39:57 -0300

    Update views.py
    importando biblioteca count para o modelo do bd django, definindo a função estatisticas para atribuir valores a noticias, importando para formato CSV e fazendo um import para http.

--- backend/noticias/views.py
+++ backend/noticias/views.py
@@ -11,3 +11,46 @@ def listar_noticias(request):
     serializer = NoticiaSerializer(noticias, many=True)
 
     return Response(serializer.data)
+
+from django.db.models import Count
+
+@api_view(['GET'])
+def estatisticas(request):
+
+    total = Noticia.objects.count()
+
+    fontes = Noticia.objects.values("fonte").annotate(total=Count("fonte"))
+
+    ultima = Noticia.objects.order_by("-data").first()
+
+    return Response({
+        "total_noticias": total,
+        "fontes": list(fontes),
+        "ultima_noticia": ultima.titulo if ultima else None
+    })
+
+import csv
+from django.http import HttpResponse
+from .models import Noticia
+
+def exportar_csv(request):
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="noticias.csv"'
+
+    writer = csv.writer(response)
+
+    writer.writerow(['ID', 'Titulo', 'Link', 'Fonte', 'Data'])
+
+    noticias = Noticia.objects.all()
+
+    for noticia in noticias:
+        writer.writerow([
+            noticia.id,
+            noticia.titulo,
+            noticia.link,
+            noticia.fonte,
+            noticia.data
+        ])
+
+    return response
