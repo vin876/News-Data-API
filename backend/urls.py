08439bc739f24a343e7967e457a7d69734627ab9
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2026-04-21 14:38:57 -0300

    Update urls.py
    Definindo as rotas da aplicação noticias, mapeando URLs para funções da views, permitindo listar notícias, visualizar estatísticas e exportar os dados em CSV;

--- backend/urls.py
+++ backend/urls.py
@@ -1,1 +1,15 @@
+from django.urls import path
+from .views import listar_noticias, estatisticas
 
+urlpatterns = [
+    path('noticias/', listar_noticias),
+    path('stats/', estatisticas),
+]
+
+from .views import listar_noticias, estatisticas, exportar_csv
+
+urlpatterns = [
+    path('noticias/', listar_noticias),
+    path('stats/', estatisticas),
+    path('export/csv/', exportar_csv),
+]
