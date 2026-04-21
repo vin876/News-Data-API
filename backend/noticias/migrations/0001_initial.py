b05e4e114a938b4d5ed0b3af4152b7094df07f5c
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2026-04-21 14:33:54 -0300

    Update 0001_initial.py
    Criar ou alterar o banco de dados;
    Identificador único;
    cresce automaticamente;
    armazena o link da notícia;
    garante que seja uma URL válida;

--- backend/noticias/migrations/0001_initial.py
+++ backend/noticias/migrations/0001_initial.py
@@ -1,1 +1,23 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Noticia',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('titulo', models.CharField(max_length=300)),
+                ('link', models.URLField()),
+                ('fonte', models.CharField(max_length=100)),
+                ('data', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
 
