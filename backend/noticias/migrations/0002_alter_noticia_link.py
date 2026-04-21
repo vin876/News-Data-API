e7c67e3e49e4237f7960abc8bbed5b296f25c5ad
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2026-04-21 14:35:23 -0300

    Update 0002_alter_noticia_link.py
    Altera o campo link do modelo Noticia, sendo unique no banco de dados, para garantir que não existam notícias duplicadas com o mesmo link.

--- backend/noticias/migrations/0002_alter_noticia_link.py
+++ backend/noticias/migrations/0002_alter_noticia_link.py
@@ -1,1 +1,16 @@
+from django.db import migrations, models
 
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('noticias', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='noticia',
+            name='link',
+            field=models.URLField(unique=True),
+        ),
+    ]
